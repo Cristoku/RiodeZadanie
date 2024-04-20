306b91b089f8cd7ddc852e40d4c139ff5e101356
Cristoku <daniel.krzyszczuk1@gmail.com>
2024-04-20 11:08:57 +0200

    12

--- RiodeZadanie/Program.cs
+++ RiodeZadanie/Program.cs
@@ -1,59 +1,49 @@
-﻿string test = @"5
-5 1 2 4 3 
-0";
-Console.SetIn(new StringReader(test));
-
-while (true)
+﻿while (true)
 {
     int n = int.Parse(Console.ReadLine());
     if (n == 0) break;
 
     var linia = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
     Solution(n, linia);
-
-    //if (n > 1000)
-    //{
-    //    Console.WriteLine("Number of trucks cannot be more than 1000.");
-    //    continue;
-    //}
-
-    //string[] input = Console.ReadLine().Split();
-    //int[] order = new int[n];
-    //for (int i = 0; i < n; i++)
-    //{
-    //    order[i] = int.Parse(input[i]);
-    //}
-
-    //if (Solution(order))
-    //{
-
-    //    Console.WriteLine("yes");
-    //}
-    //else
-    //{
-    //    Console.WriteLine("no");
-    //}
 }
 
 
 static void Solution(int n, string[] linia)
 {
     Queue<int> wjazd = new Queue<int>(Array.ConvertAll(linia, int.Parse));
-    Queue<int> wyjazd = new Queue<int>();
     Stack<int> uliczka = new Stack<int>();
-
     int oczekiwany = 1;
-    if (wjazd.Peek() == oczekiwany)
+
+    while (wjazd.Count > 0)
     {
-        wyjazd.Enqueue(wjazd.Dequeue());
+        if (wjazd.Peek() == oczekiwany)
+        {
+            wjazd.Dequeue();
+            oczekiwany++;
+        }
+        else if (uliczka.Count > 0 && uliczka.Peek() == oczekiwany)
+        {
+            uliczka.Pop();
+            oczekiwany++;
+        }
+        else
+        {
+            uliczka.Push(wjazd.Dequeue());
+        }
+    }
+
+    while (uliczka.Count > 0 && uliczka.Peek() == oczekiwany)
+    {
+        uliczka.Pop();
         oczekiwany++;
+    }
+
+    if (oczekiwany == n + 1)
+    {
+        Console.WriteLine("yes");
     }
     else
     {
-        uliczka.Push(wjazd.Dequeue());
+        Console.WriteLine("no");
     }
-
 }
-
-
-
